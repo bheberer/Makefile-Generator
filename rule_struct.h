e57d515b2f3b4e6b56af5f9f263bb054d903b618
gibson543 <billyheb@aol.com>
2017-07-16 17:14:38 -0700

    commented

--- rule_struct.h
+++ rule_struct.h
@@ -1,8 +1,12 @@
+// Node structure that represents a dependency. Contains the name of the
+// dependency and a pointer to the next dependency in the list.
 struct Node {
   char *name;
   struct Node *next;
 };
 
+// Structure defining a rule of the Makefile. Contains the rule target and
+// a linked list of dependencies for that rule.
 typedef struct Rule {
   char *target;
   struct Node *deps;
